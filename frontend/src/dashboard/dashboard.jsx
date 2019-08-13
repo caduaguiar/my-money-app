089908634ba3ee3b8209c3ddf4e4a901dc0cb68d
Cadu Aguiar <caduaguiar.roma@gmail.com>
2019-08-12 23:49:15 -0300

    final layout

--- frontend/src/dashboard/dashboard.jsx
+++ frontend/src/dashboard/dashboard.jsx
@@ -3,7 +3,7 @@ import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 
 import { getSummary } from './dashboardActions'
-import ContentHeader from '../common/template/contentHeadr'
+import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
 import ValueBox from '../common/widget/valueBox'
 import Row from '../common/layout/row'
